6a4234b3e0f9c7b202191fd654e84a5012e18f7c
sfujiwara <shuhei.fujiwara@gmail.com>
2016-11-06 21:59:59 +0900

    use simple chasing simulator

--- dqn.py
+++ dqn.py
@@ -15,41 +15,28 @@ logger.addHandler(logging.StreamHandler())
 
 class DQN:
 
-    def __init__(self, input_size=84, learning_rate=1e-3):
+    def __init__(self, input_size=84, learning_rate=1e-4, n_actions=5):
         self.learning_rate = learning_rate
+        self.n_actions = n_actions
         self.gamma = 0.95
-        self.x_ph = tf.placeholder(tf.float32, shape=[None, input_size**2], name="x_placeholder")
+        self.x_ph = tf.placeholder(tf.float32, shape=[None, input_size], name="x_placeholder")
         self.y_ph = tf.placeholder(tf.float32, shape=[None], name="y_placeholder")
         self.a_ph = tf.placeholder(tf.int64, shape=[None], name="a_placeholder")
-        self.q = self._inference(self.x_ph)
+        self.q = self._inference(self.x_ph, self.n_actions)
         self.loss = self._build_loss(self.y_ph, self.q, self.a_ph)
         self.train_ops = self._build_optimizer(self.loss, learning_rate)
         self.merged = tf.merge_all_summaries()
 
     @staticmethod
-    def _inference(x_ph):
-        with tf.name_scope("q_network/hidden1"):
-            # Hidden layer 1
-            w_fc1 = tf.Variable(tf.truncated_normal([x_ph.get_shape()[1].value, 8]))
-            b_fc1 = tf.Variable(tf.zeros([8]))
-            h_fc1 = tf.nn.relu(tf.nn.bias_add(tf.matmul(x_ph, w_fc1), b_fc1))
-            # Summaries
-            tf.histogram_summary(w_fc1.name, w_fc1)
-            tf.histogram_summary(b_fc1.name, b_fc1)
-        with tf.name_scope("q_network/output"):
-            # Hidden layer 2
-            w_out = tf.Variable(tf.truncated_normal([8, 5]))
-            b_out = tf.Variable(tf.zeros([5]))
-            outputs = tf.nn.bias_add(tf.matmul(h_fc1, w_out), b_out)
-            # Summaries
-            tf.histogram_summary(w_out.name, w_out)
-            tf.histogram_summary(b_out.name, b_out)
+    def _inference(x_ph, n_actions):
+        logits = tf.contrib.layers.fully_connected(x_ph, n_actions, activation_fn=None)
+        outputs = tf.nn.softmax(logits)
         return outputs
 
     @staticmethod
     def _build_loss(y_t_ph, q_t, a_ph):
         with tf.name_scope("loss"):
-            a_t_one_hot = tf.one_hot(a_ph, 5)
+            a_t_one_hot = tf.one_hot(a_ph, q_t.get_shape()[1].value)
             q_t_acted = tf.reduce_sum(q_t * a_t_one_hot, reduction_indices=1)
             loss = tf.squared_difference(q_t_acted, y_t_ph)
         return loss
--- trainer/task.py
+++ trainer/task.py
@@ -8,15 +8,18 @@ import tensorflow as tf
 import dqn
 import repmem
 from simulator import chasing
+from simulator import simplechasing
 
-N_INPUTS = 2
-N_ITER = 500000
+N_INPUTS = 5
+N_ITER = 20000
 LEARNING_RATE = 1e-3
+N_ACTIONS = 2
 
 # Create DQN agent
-dqn_agent = dqn.DQN(input_size=N_INPUTS, learning_rate=LEARNING_RATE)
+dqn_agent = dqn.DQN(input_size=N_INPUTS, learning_rate=LEARNING_RATE, n_actions=2)
 # Create game simulator
-chasing_simulator = chasing.ChasingSimulator(field_size=N_INPUTS)
+# chasing_simulator = chasing.ChasingSimulator(field_size=N_INPUTS)
+chasing_simulator = simplechasing.SimpleChasingSimulator()
 # Create replay memory
 replay_memory = repmem.ReplayMemory()
 
@@ -27,9 +30,12 @@ with tf.Session() as sess:
     # Initializer
     sess.run(tf.initialize_all_variables())
     win_count = 0.
+    game_count = 1e-5
     for i in range(N_ITER):
-        if i < 10000:
-            action = np.random.randint(5)
+        # print chasing_simulator.draw_field()
+        if i < 5000:
+            action = 0
+            # action = np.random.randint(N_ACTIONS)
         else:
             action = np.argmax(dqn_agent.act(sess, x_t))
             if np.random.uniform() >= 0.8:
@@ -40,20 +46,22 @@ with tf.Session() as sess:
         terminal = np.atleast_1d(res["terminal"])
         r_t = np.atleast_1d(res["reward"])
         a_t = np.atleast_1d(res["action"])
-        replay_memory.store(x_t[0], a_t[0], r_t[0], x_t_plus_1[0], terminal[0])
-        mini_batch = replay_memory.sample(size=32)
-        train_loss = dqn_agent.update(
-            sess,
-            mini_batch["s_t"],
-            mini_batch["a_t"],
-            mini_batch["r_t"],
-            mini_batch["s_t_plus_1"],
-            mini_batch["terminal"]
-        )
-        if terminal:
-            if r_t > 1 - 1e-5:
-                win_count += 1
-        win_count += terminal
-        if i % 1000 == 0:
-            # summary_writer.add_summary(dqn_agent.write_summary(sess), i)
-            print("iter: {0} win_rate: {1} reward: {2} loss: {3}".format(i, win_count/(i+1), r_t, np.mean(train_loss)))
+        if r_t > -0.1:
+            replay_memory.store(x_t[0], a_t[0], r_t[0], x_t_plus_1[0], terminal[0])
+        if i > 100:
+            mini_batch = replay_memory.sample(size=252)
+            train_loss = dqn_agent.update(
+                sess,
+                mini_batch["s_t"],
+                mini_batch["a_t"],
+                mini_batch["r_t"],
+                mini_batch["s_t_plus_1"],
+                mini_batch["terminal"]
+            )
+            if terminal:
+                # print chasing_simulator.draw_field()
+                if r_t > 1 - 1e-5:
+                    win_count += 1
+                # print "game start"
+                game_count += 1
+                print("iter: {0} win_rate: {1} reward: {2} loss: {3}".format(i, win_count / game_count, r_t, np.mean(train_loss)))
