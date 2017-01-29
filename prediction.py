207225191b0ae1e91eb9e544fe941037cf8aacd3
sfujiwara <shuhei.fujiwara@gmail.com>
2017-01-29 15:05:17 +0900

    modify prediction.py to use Cloud Storage

--- prediction.py
+++ prediction.py
@@ -2,19 +2,25 @@
 
 # Default modules
 import json
+import os
 
 # Additional modules
 import numpy as np
 import tensorflow as tf
 
-FIELD_SIZE = 4
+FIELD_SIZE = 8
+MODEL_DIR = "gs://cpb100demo1-ml/dqn/dqn20161202170846/model"
 
 with tf.Graph().as_default() as graph:
-    saver = tf.train.import_meta_graph("log/model/export.meta")
+    saver = tf.train.import_meta_graph(os.path.join(MODEL_DIR, "export.meta"))
     inputs = json.loads(tf.get_collection("inputs")[0])
     outputs = json.loads(tf.get_collection("outputs")[0])
     state = graph.get_tensor_by_name(inputs["state"])
     q = graph.get_tensor_by_name(outputs["q"])
-    with tf.Session() as sess:
-        saver.restore(sess, "log/model/export")
+    # Create session
+    sess = tf.Session()
+    saver.restore(sess, os.path.join(MODEL_DIR, "export"))
+    for _ in range(10):
         print sess.run(q, feed_dict={state: np.zeros([1, FIELD_SIZE, FIELD_SIZE, 3])})
+    # Close session
+    sess.close()
