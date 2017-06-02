d73e80e8eaecbc4d6e528bcf1109e3634b8b6255
sfujiwara <shuhei.fujiwara@gmail.com>
2017-06-02 11:52:49 +0900

    add shell scripts

--- shellscripts/metaltile_local.sh
+++ shellscripts/metaltile_local.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+
+ENV_NAME="Chasing-v1"
+
+python -m trainer.task --output_path outputs/${ENV_NAME} \
+                       --n_episodes 5000 \
+                       --learning_rate 0.001 \
+                       --n_updates 5 \
+                       --batch_size 50 \
+                       --field_size 84 \
+                       --env_name ${ENV_NAME}
